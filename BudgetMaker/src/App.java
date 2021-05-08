54f072d6fabf63c220f04ed82efb007c732aee80
Abbas Saeed Hasan <ahasan62@uwo.ca>
2021-05-09 02:59:40 +0400

    First commit

---
+++ BudgetMaker/src/App.java
@@ -0,0 +1,47 @@
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class App extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+    	primaryStage.setTitle("Budget Maker");
+    	
+    	primaryStage.setScene(new Scene(root));
+    	primaryStage.show();
+
+    	
+//        Button btn = new Button();
+//        btn.setText("Say 'Hello World'");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+// 
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+//        
+//        StackPane root = new StackPane();
+//        root.getChildren().add(btn);
+//
+//        Scene scene = new Scene(root, 300, 250);
+//
+//        primaryStage.setTitle("Hello World!");
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+    }
+ public static void main(String[] args) {
+        launch(args);
+    }
+}
